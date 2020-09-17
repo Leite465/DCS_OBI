3220d728261aa939baac411b8e7bb6d875bae261
leite465 <gabriel-leite2014@outlook.com>
2020-09-17 19:49:50 -0300

    yee

--- OBI.lua
+++ OBI.lua
@@ -1,3 +1,6 @@
+msg = MESSAGE:New("Test",10)
+msg:ToAll()
+
 do -- Setup the Command Centers
   
   RU_CC = COMMANDCENTER:New( GROUP:FindByName( "REDHQ" ), "Russia HQ" )
@@ -6,43 +9,49 @@ do -- Setup the Command Centers
 end
 --spawn the Awacs aircraft for both coalitions
 R1AwacsSpawn = SPAWN:New( "RU Awacs 1" )
-	:InitLimit(1,2)
-	:InitRepeatOnLanding()
-	:OnSpawnGroup(
-	function A1Group:OnEventCrash( EventData )  --start of eventhandling
-        A1Group:Respawn()                             
-    end  -- end of eventhandling
-	)
+    :InitRepeatOnLanding()
+    :OnSpawnGroup(
+        function (SpawnGroup)
+            function SpawnGroup:OnEventCrash( EventData )
+                SpawnGroup:Respawn()
+            end
+        end
+    )
 R1AwacsSpawn:Spawn()
 
 R2AwacsSpawn = SPAWN:New( "RU Awacs 2" )
-	:InitLimit(1,2)
+	--:InitLimit(1,2)
 	:InitRepeatOnLanding()
 	:OnSpawnGroup(
-	function A2Group:OnEventCrash( EventData )  --start of eventhandling
-        A2Group:Respawn()                             
-    end  -- end of eventhandling
-	)
+        function (SpawnGroup)
+            function SpawnGroup:OnEventCrash( EventData )
+                SpawnGroup:Respawn()
+            end
+        end
+    )
 R2AwacsSpawn:Spawn()
 
 U1AwacsSpawn = SPAWN:New( "US Awacs 1" )
-	:InitLimit(1,2)
-	:InitRepeatOnLanding()
-	:OnSpawnGroup(
-	function U1Group:OnEventCrash( EventData )  --start of eventhandling
-        U1Group:Respawn()                             
-    end  -- end of eventhandling
-	)
+    :InitRepeatOnLanding()
+    :OnSpawnGroup(
+        function (SpawnGroup)
+            function SpawnGroup:OnEventCrash( EventData )
+                SpawnGroup:Respawn()
+            end
+        end
+    )
 U1AwacsSpawn:Spawn()
 
 U2AwacsSpawn = SPAWN:New( "US Awacs 2" )
-	:InitLimit(1,2)
+	--:InitLimit(1,2)
 	:InitRepeatOnLanding()
 	:OnSpawnGroup(
-	function U1Group:OnEventCrash( EventData )  --start of eventhandling
-        U1Group:Respawn()                             
-    end  -- end of eventhandling
-	)
+        function (SpawnGroup)
+            function SpawnGroup:OnEventCrash( EventData )
+                SpawnGroup:Respawn()
+            end
+        end
+    )
 U2AwacsSpawn:Spawn()
 
 --Add the EWR and Aircraft to the A2A_Dispatcher defense network
@@ -63,8 +72,19 @@ A2ADispatcher_Red:SetCommandCenter( RU_CC )
 A2ADispatcher_Blue:SetCommandCenter( US_CC )
 
 
+
+
+
+
+
+
+
+
+
+
+
 CapSpawn = SPAWN:New( "Rus1" )
-    :InitLimit(6,12)
+    --:InitLimit(6,12)
     :InitRepeatOnLanding()                                                               
     :OnSpawnGroup(                                                                 
         function ( SpawnGroup )
@@ -82,8 +102,8 @@ CapSpawn = SPAWN:New( "Rus1" )
     )
 CapSpawn:Spawn()
 
-CapKSpawn = SPAWN:New( "RusK" ) --1495088-8
-    :InitLimit(6,12)
+CapKSpawn = SPAWN:New( "RusK" )
+    --:InitLimit(6,12)
     :InitRepeatOnLanding()                                                               
     :OnSpawnGroup(                                                                 
         function ( SpawnGroup )
@@ -100,23 +120,3 @@ CapKSpawn = SPAWN:New( "RusK" ) --1495088-8
     end
     )
 CapKSpawn:Spawn()
-
-Cas1Spawn = SPAWN:New( "RusCAS" )
-    :InitLimit(6,12)
-    :InitRepeatOnLanding()                                                               
-    :OnSpawnGroup(                                                                 
-        function ( Cas1Spawn )
-        CASEngagementZone1 = ZONE:New( "Frontline1" ) -- Start of the Cas setup
-        RuCasZone = AI_CAS_ZONE:New( PatrolZone, 500, 1000, 500, 600, CASEngagementZone1 )
-        Targets = GROUP:FindByName("NATO frontline units")
-		AICasZone:SetControllable( Cas1Spawn )		
-        AICasZone:__Start( 1 ) --end of the Cas setup
-        function Cas1Spawn:OnEventCrash( EventData )  --start of eventhandling
-            if Cas1Spawn:GetSize() == 1 then
-                Cas1Spawn:Respawn()
-            end                      
-        end  -- end of eventhandling
-    end
-    )
-Cas1Spawn:Spawn()
-
